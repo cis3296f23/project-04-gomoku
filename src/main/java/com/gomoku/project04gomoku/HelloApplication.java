0f03a4a920ae828a4b0f797758fb022d1b229563
shibatachun <q1093677764@gmail.com>
2023-11-04 07:32:43 +0800

    Initial setting for menu GUI

--- src/main/java/com/gomoku/project04gomoku/HelloApplication.java
+++ src/main/java/com/gomoku/project04gomoku/HelloApplication.java
@@ -11,7 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
