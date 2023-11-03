1e284b6babd558b97a668aa2c7472c547edb4226
Yuxuan Zhu <123014326+tun71427@users.noreply.github.com>
2023-11-02 22:01:29 -0400

    Added JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.gomoku.project04gomoku {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.gomoku.project04gomoku to javafx.fxml;
+    exports com.gomoku.project04gomoku;
+}
