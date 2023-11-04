8ea278fe2b7244ba374711eeb3e30293bb157c80
shibatachun <q1093677764@gmail.com>
2023-11-04 08:30:42 +0800

    Added background Image and Style the buttons

--- src/main/java/com/gomoku/project04gomoku/HelloApplication.java
+++ src/main/java/com/gomoku/project04gomoku/HelloApplication.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
         stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
