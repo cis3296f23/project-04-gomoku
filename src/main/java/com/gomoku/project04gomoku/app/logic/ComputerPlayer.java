ac0ff5415e157f9044ac5753470901391e1b4ea8
Yuxuan Zhu <123014326+tun71427@users.noreply.github.com>
2023-11-19 18:16:51 -0500

    Adding logic for ComputerPlayer

--- src/main/java/com/gomoku/project04gomoku/app/logic/ComputerPlayer.java
+++ src/main/java/com/gomoku/project04gomoku/app/logic/ComputerPlayer.java
@@ -1,4 +1,21 @@
 package com.gomoku.project04gomoku.app.logic;
 
 public class ComputerPlayer implements Player {
+    private PlayerColor color;
+
+    public ComputerPlayer(PlayerColor color) {
+        this.color = color;
+    }
+
+    @Override
+    public PlayerColor getColor() {
+        return color;
+    }
+
+    @Override
+    public PlayerType getType() {
+        return PlayerType.COMPUTER;
+    }
+
+    // Additional methods specific to ComputerPlayer can be added here
 }
