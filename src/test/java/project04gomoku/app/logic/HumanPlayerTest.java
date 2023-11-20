f725e8b5717cbc3e3ce1b7b2a9071b9e64f31fea
Yuxuan Zhu <123014326+tun71427@users.noreply.github.com>
2023-11-19 19:19:35 -0500

    Adding testHumanPlayerColor method

--- src/test/java/project04gomoku/app/logic/HumanPlayerTest.java
+++ src/test/java/project04gomoku/app/logic/HumanPlayerTest.java
@@ -1,4 +1,20 @@
 package project04gomoku.app.logic;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.gomoku.project04gomoku.app.logic.Player;
+import com.gomoku.project04gomoku.app.logic.HumanPlayer;
+
 public class HumanPlayerTest {
+
+    @Test
+    public void testHumanPlayerColor() {
+        HumanPlayer blackPlayer = new HumanPlayer(Player.PlayerColor.BLACK);
+        assertEquals("HumanPlayer should have color BLACK.", Player.PlayerColor.BLACK, blackPlayer.getColor());
+
+        HumanPlayer whitePlayer = new HumanPlayer(Player.PlayerColor.WHITE);
+        assertEquals("HumanPlayer should have color WHITE.", Player.PlayerColor.WHITE, whitePlayer.getColor());
+    }
+    
 }
