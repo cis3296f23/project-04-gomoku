4940eccb6e4de1f458464eb34a93a602eb526440
Wenjie Gao <tul42337@temple.edu>
2023-11-22 13:40:57 -0500

    Added a brief description on the Player interface's functionalities to be implemented.

--- src/main/java/com/gomoku/project04gomoku/app/logic/Player.java
+++ src/main/java/com/gomoku/project04gomoku/app/logic/Player.java
@@ -1,5 +1,8 @@
 package com.gomoku.project04gomoku.app.logic;
 
+/**
+ * The Runnable interface should be implemented by any class whose instances are intend to be a player in the Gomoku game. The class must define both the pieces' color and player type.
+ */
 public interface Player {
     // Enums can be nested inside the interface
     enum PlayerColor {
