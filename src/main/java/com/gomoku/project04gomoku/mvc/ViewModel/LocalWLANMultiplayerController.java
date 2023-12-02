4940b0bcfd4b547e755b7802ba28f0b3239650b4
Yuxuan Zhu <123014326+tun71427@users.noreply.github.com>
2023-12-01 21:29:56 -0500

    Added basic logic

--- src/main/java/com/gomoku/project04gomoku/mvc/ViewModel/LocalWLANMultiplayerController.java
+++ src/main/java/com/gomoku/project04gomoku/mvc/ViewModel/LocalWLANMultiplayerController.java
@@ -2,6 +2,9 @@ package com.gomoku.project04gomoku.mvc.ViewModel;
 
 import com.gomoku.project04gomoku.GomokuStart;
 import com.gomoku.project04gomoku.app.logic.Game;
+import com.gomoku.project04gomoku.app.models.Board;
+import com.gomoku.project04gomoku.app.logic.Player;
+import com.gomoku.project04gomoku.app.models.Net;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,72 +14,75 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-public class LocalWLANMultiplayerController {
+public class LocalWLANMultiplayerController implements Net.NetStateChange {
     @FXML
     private Canvas canvas; // The canvas for drawing the game board and pieces
-    // Adding Start button in fxml later
     FXMLLoader fxmlLoader;
 
-    @FXML private Button RestartButton;
-    @FXML private Button SettingButton;
-    @FXML private Button BackButton;
+    @FXML
+    private Button RestartButton;
+    @FXML
+    private Button SettingButton;
+    @FXML
+    private Button BackButton;
+    @FXML
+    private Button ConnectButton;
+    @FXML
+    private Button StartButton;
+    @FXML
+    private Button SendButton;
+    @FXML
+    private Button UndoButton;
+    @FXML
+    private TextField tfIP;
+    @FXML
+    private TextField tfMessage;
+    @FXML
+    private TextArea taContent;
+    @FXML
+    private Label lbIP;
 
     private ChessUtils ChessUtils;
     private Game game;
+    private final double padding = 20;
     private GraphicsContext gc; // The graphics context for drawing on the canvas
-    private final double padding = 20; // You can adjust the padding size as needed
+
+    private Net server;
+    private Net client;
+
+    public static final String NET = "net";
+    public static final String MSG = "msg";
+    public static final String CHESS = "chess";
+    public static final String GAME = "game";
+    public static final String UNDO = "undo";
+    public static final String RESTART = "restart";
+    public static final String OK = "ok";
+    public static final String NO = "no";
+
+    public static NetType netType;
+
+    private boolean isMyTurn = true;
+
+
+    public enum NetType {
+        CLIENT, SERVER
+    }
 
     // Initialize the game and graphics context, and draw the empty game board
     public void initialize() {
         this.game = new Game();
         this.gc = canvas.getGraphicsContext2D();
-        this.ChessUtils =new ChessUtils(canvas,game);
-    }
-
-    @FXML
-    public void Replay(ActionEvent event)
-    {
-        //TODO: Replay function
-    }
-
-    @FXML
-    public void restartGame() {
-        game.restartGame();
-        ChessUtils.updateBoard();
-    }
-    @FXML
-    public  void handleCanvasClick(javafx.scene.input.MouseEvent event) {
-        ChessUtils.handleCanvasClick(event);
-    }
-
-    @FXML
-    public void GoBackToMain(ActionEvent event) throws IOException {
+        this.ChessUtils = new ChessUtils(canvas, game);
         try {
-            fxmlLoader = new FXMLLoader(GomokuStart.class.getResource("view/Menu.fxml"));
-            Scene root = new Scene(fxmlLoader.load(), 800, 600);
-
-
-            Stage stage = (Stage) BackButton.getScene().getWindow();
-
-
-            stage.setScene(root);
-            stage.show();
-
-        } catch (IOException e) {
+            lbIP.setText("Local IP: " + InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
-    @FXML
-    private void openSetting(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GomokuStart.class.getResource("view/Setting.fxml"));
-        fxmlLoader.setController(new SettingController());
-        Parent root = fxmlLoader.load();
-        Stage Setting = new Stage();
-        Setting.setTitle("Setting");
-        Setting.setScene(new Scene(root));
-        Setting.show();
-    }
-}
