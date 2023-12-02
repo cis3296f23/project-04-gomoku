f4526902e0f0c472a7d2bd910a50fc4b53785ac8
Yuxuan Zhu <123014326+tun71427@users.noreply.github.com>
2023-12-01 21:12:42 -0500

    Added a new class named Net

---
+++ src/main/java/com/gomoku/project04gomoku/app/models/Net.java
@@ -0,0 +1,14 @@
+package com.gomoku.project04gomoku.app.models;
+
+import com.gomoku.project04gomoku.mvc.ViewModel.LocalWLANMultiplayerController;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Net {
+}
+
