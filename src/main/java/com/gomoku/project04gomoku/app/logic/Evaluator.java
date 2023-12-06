c7f24a1592ec0c17675dbe0584e280019e4b7d55
Wenjie Gao <tul42337@temple.edu>
2023-12-06 00:24:40 -0500

    Finished Javadoc.

--- src/main/java/com/gomoku/project04gomoku/app/logic/Evaluator.java
+++ src/main/java/com/gomoku/project04gomoku/app/logic/Evaluator.java
@@ -1,20 +1,55 @@
 package com.gomoku.project04gomoku.app.logic;
 
 import com.gomoku.project04gomoku.app.models.Board;
-import com.gomoku.project04gomoku.app.utils.BoardUtils;
 
 import java.util.Random;
 
+/**
+ * The `Evaluator` class provides methods for evaluating the state of a Gomoku game board.
+ * It assigns scores to different patterns and shapes on the board, helping the computer player
+ * make strategic moves.
+ */
 public class Evaluator {
+    /**
+     * Represents the score assigned to a consecutive-five pattern.
+     */
     final static int FIVE = 10000000;       // ●●●●●
+    /**
+     * Represents the score assigned to a blocked consecutive-five pattern.
+     */
     final static int BLOCKED_FIVE = FIVE;   // ●●●●●○
+    /**
+     * Represents the score assigned to a consecutive-four pattern.
+     */
     final static int FOUR = 100000;         // ●●●●
+    /**
+     * Represents the score assigned to a blocked consecutive-four pattern.
+     */
     final static int BLOCKED_FOUR = 1500; // ●●●●○ ●○●●●
+    /**
+     * Represents the score assigned to a consecutive-three pattern.
+     */
     final static int THREE = 1000;          // ●●●
+
+    /**
+     * Represents the score assigned to a blocked consecutive-three pattern.
+     */
     final static int BLOCKED_THREE = 150;   // ●●●○
+    /**
+     * Represents the score assigned to a consecutive-two pattern.
+     */
     final static int TWO = 200; // ●●
+    /**
+     * Represents the score assigned to a blocked consecutive-two pattern.
+     */
     final static int BLOCKED_TWO = 15;  // ●
+    /**
+     * Represents the score assigned to a single piece.
+     */
     final static int ONE = 10;
+    /**
+     * Represents the score assigned to a blocked single piece.
+     */
     final static int BLOCKED_ONE = 1;
 
     /*
@@ -35,66 +70,48 @@ public class Evaluator {
     //  or
     // final static int SPLIT_THREE = 1000; //
     // ●●●○ or ●●●×
+    /**
+     * Represents the score assigned to a half-open three pattern.
+     */
     final static int HALF_OPEN_THREE = 500;
     // ○●●○
+    /**
+     * Represents the score assigned to an open two pattern.
+     */
     final static int OPEN_TWO = 100;
     // ●●○ or ●●×
+
+    /**
+     * Represents the score assigned to a half-open two pattern.
+     */
     final static int HALF_OPEN_TWO = 50;
     // ●○●○
     // final static int SPLIT_TWO = 75;
-
-
-
+    /**
+     * The possible directions to evaluate patterns on the board.
+     */
     public enum DIRECTION{
         // DIAGONAL_SLASH means top right to left bottom, "/"
         // DIAGONAL_BACKSLASH means top left to right bottom, "\"
         HORIZONTAL, VERTICAL, DIAGONAL_SLASH, DIAGONAL_BACKSLASH
     }
 
+    /**
+     * The game board to be evaluated.
+     */
     public Board board;
     /*
     public Evaluator(Board board){
         this.board = board;
     }
     */
-
-    /*
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        Board board = generateRandomBoard();
-        printBoard(board);
-        System.out.println("");
-        printLine(getLine(board, 0, 0, DIRECTION.DIAGONAL_BACKSLASH));
-    }
-
-    /*
-        Debug method to generate a board filled with random pieces
+    /**
+     * Evaluates the entire game board and returns a score based on the given player's perspective.
+     *
+     * @param board          The game board to be evaluated.
+     * @param currentPlayer  The player for whom the evaluation is done.
+     * @return The total score of the board from the perspective of the given player.
      */
-    public static Board generateRandomBoard() {
-        Random rand = new Random();
-        Board board = new Board();
-        for (int i = 0; i < Board.SIZE; i++) {
-            for (int j = 0; j < Board.SIZE; j++) {
-                int value = rand.nextInt(3);
-                switch (value) {
-                    case 0:
-                        board.setCell(i, j, null);
-                        break;
-                    case 1:
-                        board.setCell(i, j, new ComputerPlayer(Player.PlayerColor.BLACK));
-                        break;
-                    case 2:
-                        board.setCell(i, j, new ComputerPlayer(Player.PlayerColor.WHITE));
-                        break;
-                    default:
-                        System.out.println("ERROR: unexpected value at generateRandomBoard()");
-                }
-            }
-        }
-
-        return board;
-    }
-
     public static int evaluateBoard(Board board, Player currentPlayer) {
         int score = 0;
         for(int i=0; i<board.SIZE; i++){
@@ -104,30 +121,15 @@ public class Evaluator {
         }
         return score;
     }
-
-    //Debugger method of getLine
-    private static void printLine(Player[] line){
-        for(int i=0; i<line.length; i++){
-            Player p = line[i];
-            if (p == null) System.out.print("- ");
-            else if (p.getColor() == Player.PlayerColor.WHITE) System.out.print("o ");
-            else System.out.print("x ");
-        }
-    }
-    public static void printBoard(Board board){
-        for (int i = 0; i < Board.SIZE; i++) {
-            for (int j = 0; j < Board.SIZE; j++) {
-                Player p = board.getCell(i,j);
-                if (p == null) System.out.print("- ");
-                else if (p.getColor() == Player.PlayerColor.WHITE) System.out.print("o ");
-                else System.out.print("x ");
-            }
-            System.out.println();
-        }
-    }
-
-
-    //return score of a point by adding all the shapes in four directions
+    /**
+     * Calculates the score of a point by analyzing its surroundings in four directions.
+     *
+     * @param board         The game board.
+     * @param x             The x-coordinate of the point.
+     * @param y             The y-coordinate of the point.
+     * @param currentPlayer The player whose perspective the evaluation is done from.
+     * @return The score of the point based on various patterns in the four directions.
+     */
     private static int evaluatePoint(Board board, int x, int y, Player currentPlayer){
         int score = 0;
 
@@ -150,22 +152,14 @@ public class Evaluator {
         score += analyzeLine(backslash, currentPlayer, null);
         return score;
     }
-
-
-    private void printBoard() {
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                Player p = board.getCell(i,j);
-                if (p == null) System.out.print("- ");
-                else if (p.getColor() == Player.PlayerColor.WHITE) System.out.print("o ");
-                else System.out.print("x ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-
+    /**
+     * Analyzes the line returned by getLine and returns a score based on the patterns found.
+     *
+     * @param line         The line of players to be analyzed.
+     * @param currentPlayer The player whose perspective the analysis is done from.
+     * @param evaluated    A 2D boolean array representing the cells that have already been evaluated.
+     * @return The score based on the patterns found in the line.
+     */
     //analyze the line returned by getLine, and returns a score
     private static int analyzeLine(Player[] line, Player currentPlayer, boolean[][] evaluated){
         if(line.length != 9){
@@ -247,114 +241,15 @@ public class Evaluator {
         return 0;
     }
 
-    private static int newAnalyzeLine(Player[] line, Player currentPlayer, boolean[][] evaluated){
-        if(line.length != 9){
-            System.out.println("ERROR! Incorrect array length at analyzeLine()");
-            return 0;
-        }
-
-        //check the shape of the line by counting the pieces on both sides starting from middle
-        int gapCount = 0;        //gapCount should be <= 1
-        int blockedCount = 0;    //blockCount should be <= 1
-        int pieceCount = 0;      //pieceCount determines the shape, should be 1 <= pieceCount <= 5
-
-        //check left side of the array from middle
-        for(int i=4; i>=0; i--){
-            if(line[i] == null && gapCount == 0){
-                gapCount++;
-            } else if(line[i] == null && gapCount >= 1){
-                gapCount++;
-                break;
-            }else if(line[i].getColor() != currentPlayer.getColor() && blockedCount == 0){  //one end blocked
-                blockedCount++;
-                break;
-            }else if(line[i].getColor() == currentPlayer.getColor()){
-                pieceCount++;
-            }else{
-                System.out.println("ERROR: unknown situation at analyzeLine()");
-            }
-        }
-
-        //check right side of the array from middle
-        for(int i=4; i<9; i++){
-            if(line[i] == null && gapCount == 0){  //problem with using ==
-                gapCount++;
-            }else if(line[i] == null && gapCount >= 1){
-                gapCount++;
-                break;
-            }else if(line[i].getColor() != currentPlayer.getColor() && blockedCount == 0){
-                blockedCount++;
-            }else if(line[i].getColor() != currentPlayer.getColor() && blockedCount >= 1 ) {  //two end blocked, use less
-                break;
-            }else if(line[i].getColor() == currentPlayer.getColor()){
-                pieceCount++;
-            }else{
-                System.out.println("ERROR: unknown situation at analyzeLine()");
-            }
-        }
-
-        return 0;
-    }
-
-    public static String newGetLine(Board board, int x, int y, DIRECTION DIRECTION, Player currentPlayer){
-        int dx = 0, dy = 0;
-        int dx2 = 0, dy2 = 0;       //two sides of direction
-
-        switch(DIRECTION){
-            case HORIZONTAL:        //if DIRECTION.HORIZONTAL, then check left to right
-                dx = 0;   dy=-1;    //left
-                dx2 = 0;  dy2=1;     //right
-                break;
-            case VERTICAL:          //if DIRECTION.VERTICAL, then check up to down
-                dx = -1;   dy = 0;  //up
-                dx2 = 1;  dy2 = 0;  //down
-                break;
-            case DIAGONAL_SLASH:    //if DIRECTION.DIAGONAL_SLASH, then check top right to bottom left
-                dx = -1;   dy = 1;  //top right
-                dx2 = 1; dy2 = -1;  //bottom left
-                break;
-            case DIAGONAL_BACKSLASH:    //if DIRECTION.DIAGONAL_BACKSLASH, then check top left to bottom right
-                dx = -1;   dy = -1;
-                dx2 = 1;   dy2 = 1;
-                break;
-        }
-
-        x = x + dx*4;  //setting the point to start [ O X X X X X X X X ]
-        y = y + dy*4;
-        String toReturn = "";
-
-        /*
-            3 = out of bound
-            2 = opponent
-            1 = self
-            0 = empty
-         */
-
-        for(int i=0; i<9; i++){
-
-            if(checkOutOfBoard(x, y)){
-                toReturn += "3";
-                continue;
-            }
-            Player piece = board.getCell(x, y);
-
-            if(piece == null) {
-                toReturn += "0";
-            }else if(piece.getColor() == currentPlayer.getColor()){
-                toReturn += "1";
-            }else if(piece.getColor() != currentPlayer.getColor()){
-                toReturn += "2";
-            }else{
-                System.out.println("ERROR! unexpected situation at newGetLine()");
-            }
-
-            x += dx;
-            y += dy;
-        }
-        return null;
-    }
-
-    //return a line (array of size 9) given the location and direction
+    /**
+     * Returns a line (array of size 9) given the location and direction on the game board.
+     *
+     * @param board     The game board.
+     * @param x         The x-coordinate of the starting point.
+     * @param y         The y-coordinate of the starting point.
+     * @param DIRECTION The direction in which the line is formed (HORIZONTAL, VERTICAL, DIAGONAL_SLASH, DIAGONAL_BACKSLASH).
+     * @return An array of players representing the line formed in the specified direction.
+     */
     public static Player[] getLine(Board board, int x, int y, DIRECTION DIRECTION){
         Player[] toReturn = new Player[9];  // stretch 4 steps from the starting point on each side of direction,
                                             // resulting in an array of length 9
@@ -400,8 +295,13 @@ public class Evaluator {
         return toReturn;
     }
 
-    //return true for out of bound
-    //return false for NOT out of bound
+    /**
+     * Checks if a given point is out of bounds on the game board.
+     *
+     * @param x The x-coordinate of the point.
+     * @param y The y-coordinate of the point.
+     * @return {@code true} if the point is out of bounds, {@code false} otherwise.
+     */
     public static boolean checkOutOfBoard(int x, int y){
         return x < 0 || y < 0 || x >= Board.SIZE || y >= Board.SIZE;
     }
